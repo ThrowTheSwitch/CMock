b29120fa85ff69d7a0666ffa4a67775bf832a753
Dennis Lambe Jr <malsyned@malsyned.net>
2012-12-19 14:21:30 -0500

    Added system test case for module name sanitizing

---
+++ test/system/test_compilation/0_strange-filename+.h
@@ -0,0 +1,7 @@
+/* ==========================================
+    CMock Project - Automatic Mock Generation for C
+    Copyright (c) 2007 Mike Karlesky, Mark VanderVoord, Greg Williams
+    [Released under MIT License. Please refer to license.txt for details]
+========================================== */
+
+int ordinary_function_strange_filename(char *c);
