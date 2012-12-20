86a664e4f667b97b8ee924c373f1842b7090f878
Dennis Lambe Jr <malsyned@malsyned.net>
2012-12-20 18:06:48 -0500

    Added unit test for new CMock_Guts_MemEndOfChain() C function

--- test/c/TestCMockC_Runner.c
+++ test/c/TestCMockC_Runner.c
@@ -15,6 +15,7 @@ extern void test_MemNewWillReturnNullIfGivenIllegalSizes(void);
 extern void test_MemChainWillReturnNullAndDoNothingIfGivenIllegalInformation(void);
 extern void test_MemNextWillReturnNullIfGivenABadRoot(void);
 extern void test_ThatWeCanClaimAndChainAFewElementsTogether(void);
+extern void test_MemEndOfChain(void);
 extern void test_ThatCMockStopsReturningMoreDataWhenItRunsOutOfMemory(void);
 extern void test_ThatCMockStopsReturningMoreDataWhenAskForMoreThanItHasLeftEvenIfNotAtExactEnd(void);
 extern void test_ThatWeCanAskForAllSortsOfSizes(void);
@@ -28,6 +29,7 @@ int main(void)
   RUN_TEST(test_MemChainWillReturnNullAndDoNothingIfGivenIllegalInformation, 32);
   RUN_TEST(test_MemNextWillReturnNullIfGivenABadRoot, 46);
   RUN_TEST(test_ThatWeCanClaimAndChainAFewElementsTogether, 57);
+  RUN_TEST(test_MemEndOfChain, 282);
   RUN_TEST(test_ThatCMockStopsReturningMoreDataWhenItRunsOutOfMemory, 139);
   RUN_TEST(test_ThatCMockStopsReturningMoreDataWhenAskForMoreThanItHasLeftEvenIfNotAtExactEnd, 185);
   RUN_TEST(test_ThatWeCanAskForAllSortsOfSizes, 233);
