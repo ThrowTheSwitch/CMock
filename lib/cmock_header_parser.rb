a55efdba1908d36d85712120258955025da4f0d9
Andrew Setter <ajsetter@gmail.com>
2017-03-03 13:12:04 -0500

    Improve handling of brackets in function params
    During header parsing, #define substitutions in fixed-length array
    function parameters can have parentheses in them which can cause a
    failure in the bracket->asterisk conversion that happens during the
    mocking process. This change updates the regex to also match for
    parentheses in the function parameter brackets to ensure they get
    swapped out.

--- lib/cmock_header_parser.rb
+++ lib/cmock_header_parser.rb
@@ -187,9 +187,9 @@ class CMockHeaderParser
       return 'void'
     else
       c=0
-      arg_list.gsub!(/(\w+)(?:\s*\[[\s\d\w+-]*\])+/,'*\1')  # magically turn brackets into asterisks
-      arg_list.gsub!(/\s+\*/,'*')                           # remove space to place asterisks with type (where they belong)
-      arg_list.gsub!(/\*(\w)/,'* \1')                       # pull asterisks away from arg to place asterisks with type (where they belong)
+      arg_list.gsub!(/(\w+)(?:\s*\[\(*[\s\d\w+-]*\)*\])+/,'*\1')  # magically turn brackets into asterisks, also match for parentheses that come from macros
+      arg_list.gsub!(/\s+\*/,'*')                                 # remove space to place asterisks with type (where they belong)
+      arg_list.gsub!(/\*(\w)/,'* \1')                             # pull asterisks away from arg to place asterisks with type (where they belong)
 
       #scan argument list for function pointers and replace them with custom types
       arg_list.gsub!(/([\w\s\*]+)\(+\s*\*[\*\s]*([\w\s]*)\s*\)+\s*\(((?:[\w\s\*]*,?)*)\s*\)*/) do |m|
