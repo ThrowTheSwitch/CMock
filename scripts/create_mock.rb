56515a22922223d669f140da440cc1fab03abe65
Thomas De Backer <tdebacker@nalys-group.com>
2019-10-11 15:57:14 +0200

    fix empty cmockconf variable

--- scripts/create_mock.rb
+++ scripts/create_mock.rb
@@ -4,6 +4,11 @@ raise "Header file to mock must be specified!" unless ARGV.length >= 1
 
 mock_out = ENV.fetch('MOCKS_DIR', './build/test/mocks')
 mock_prefix = ENV.fetch('MOCK_PREFIX', 'mock_')
-cmock_config = ENV.fetch('CMOCK_CONFIG', {:plugins => [:ignore, :return_thru_ptr], :mock_prefix => mock_prefix, :mock_path => mock_out})
+
+cmock_config = ENV.fetch('CMOCK_CONFIG', "")
+if cmock_config.empty?
+  cmock_config = {:plugins => [:ignore, :return_thru_ptr], :mock_prefix => mock_prefix, :mock_path => mock_out}
+end
+
 cmock = CMock.new(cmock_config)
 cmock.setup_mocks(ARGV[0])
