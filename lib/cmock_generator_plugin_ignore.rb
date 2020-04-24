7bfa02cd239a23ec32abc092353fcabb9467f456
pwatt01 <pwatt01@hotmail.com>
2020-04-24 21:49:16 +0930

    Fix whitespace errors, minimize unnecessary branching

--- lib/cmock_generator_plugin_ignore.rb
+++ lib/cmock_generator_plugin_ignore.rb
@@ -24,10 +24,10 @@ class CMockGeneratorPluginIgnore
 
   def mock_function_declarations(function)
     if function[:return][:void?]
-    lines =   "#define #{function[:name]}_Ignore() #{function[:name]}_CMockIgnore()\n" \
+      lines =   "#define #{function[:name]}_Ignore() #{function[:name]}_CMockIgnore()\n" \
              "void #{function[:name]}_CMockIgnore(void);\n"
     else
-     lines =  "#define #{function[:name]}_IgnoreAndReturn(cmock_retval) #{function[:name]}_CMockIgnoreAndReturn(__LINE__, cmock_retval)\n" \
+      lines =  "#define #{function[:name]}_IgnoreAndReturn(cmock_retval) #{function[:name]}_CMockIgnoreAndReturn(__LINE__, cmock_retval)\n" \
              "void #{function[:name]}_CMockIgnoreAndReturn(UNITY_LINE_TYPE cmock_line, #{function[:return][:str]});\n"
     end
     
@@ -68,13 +68,12 @@ class CMockGeneratorPluginIgnore
     lines << "}\n\n"
   
     #add stop ignore function. it does not matter if there are any args
-    lines << "void #{function[:name]}_CMockStopIgnore(void)\n{\n"
-    lines << "  if(Mock.#{function[:name]}_IgnoreBool) {\n"
-    lines << "    Mock.#{function[:name]}_IgnoreBool = (char)0;\n"
+    lines << "void #{function[:name]}_CMockStopIgnore(void)\n  {\n"
     unless function[:return][:void?]
+      lines << "  if(Mock.#{function[:name]}_IgnoreBool) \n"
       lines << "    Mock.#{function[:name]}_CallInstance = CMock_Guts_MemNext(Mock.#{function[:name]}_CallInstance);\n"
     end
-    lines << "  }\n"
+    lines << "  Mock.#{function[:name]}_IgnoreBool = (char)0;\n"
     lines << "}\n\n"
   end
 
